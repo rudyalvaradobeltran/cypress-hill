4091ec631e4409da6334d6d31fc63b518b849a9b
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-12-01 21:08:59 -0300

    Project-2

---
+++ project-2/client/cypress/integration/rewards.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('rewards dashboard', () => {
+  beforeEach(() => {
+    cy.visit('/rewards')
+  })
+
+  it('should display a list of rewards', () => {
+    cy.get('ul')
+      .should('contain', '500 points for drinking 8 cups of water for 7 straight days')
+      .and('contain', '850 points for fasting for 5 days straight')
+  })
+
+  it('should display a list of rewards with mock', () => {
+    cy.intercept('GET', 'http://localhost:3000/rewards', {
+      fixture: 'rewards.json'
+    })
+    cy.get('ul')
+      .should('contain', '500 points for drinking 8 cups of water for 7 straight days')
+      .and('contain', '850 points for fasting for 5 days straight')
+  })
+})
